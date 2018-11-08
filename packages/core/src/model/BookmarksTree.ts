2fa64327f8dc3b9a62d2525855a4248bf502db06
Chabanois Cédric <cchabanois@gmail.com>
2018-11-08 21:53:40 +0100

    Added new methods to BookmarksTree and tests

--- packages/core/src/model/BookmarksTree.ts
+++ packages/core/src/model/BookmarksTree.ts
@@ -30,7 +30,7 @@ export class BookmarksTree {
     public addBookmarks(parentId: BookmarkId, bookmarks: Iterable<Bookmark>, index?: number): BookmarksTree {
         this.checkBookmarkFolderExist(parentId);
         this.checkBookmarksNotInTree(bookmarks);
-        let bookmarksMap = this.bookmarksMap.withMutations(map=>{
+        let bookmarksMap = this.bookmarksMap.withMutations(map => {
             for (let bookmark of bookmarks) {
                 map = map.set(bookmark.bookmarkId, bookmark);
             }
@@ -45,7 +45,7 @@ export class BookmarksTree {
         return this.bookmarksMap.get(bookmarkId);
     }
 
-    public getBookmarkFolder(bookmarkId : BookmarkId) : BookmarkFolder | undefined {
+    public getBookmarkFolder(bookmarkId: BookmarkId): BookmarkFolder | undefined {
         const bookmark = this.bookmarksMap.get(bookmarkId);
         if (bookmark instanceof BookmarkFolder) {
             return bookmark as BookmarkFolder;
@@ -68,8 +68,94 @@ export class BookmarksTree {
         }
     }
 
+    public setPropertyValue(bookmarkId: BookmarkId, propertyName: string, propertyValue: string | undefined) : BookmarksTree {
+        let bookmark = this.checkBookmarkExist(bookmarkId);
+        let newProperties: Map<string, string>;
+        if (propertyValue === undefined) {
+            newProperties = bookmark.properties.delete(propertyName);
+        } else {
+            if (propertyValue !== bookmark.properties.get(propertyName)) {
+                newProperties = bookmark.properties.set(propertyName, propertyValue);
+            } else {
+                newProperties = bookmark.properties;
+            }
+        }
+        if (newProperties === bookmark.properties) {
+            return this;
+        }
+        if (bookmark instanceof BookmarkFolder) {
+            bookmark = new BookmarkFolder(bookmark.bookmarkId, newProperties);
+        } else {
+            bookmark = new Bookmark(bookmark.bookmarkId, newProperties);
+        }
+        const newBookmarksMap = this.bookmarksMap.set(bookmark.bookmarkId, bookmark);
+        return new BookmarksTree(this.rootFolderId, newBookmarksMap, this.childrenMap, this.parentMap);
+    }
+
+    public setProperties(bookmarkId: BookmarkId, properties : Map<string, string>) : BookmarksTree {
+        let bookmark = this.checkBookmarkExist(bookmarkId);
+        if (bookmark.properties.equals(properties)) {
+            return this;
+        }
+        if (bookmark instanceof BookmarkFolder) {
+            bookmark = new BookmarkFolder(bookmark.bookmarkId, properties);
+        } else {
+            bookmark = new Bookmark(bookmark.bookmarkId, properties);
+        }
+        const newBookmarksMap = this.bookmarksMap.set(bookmark.bookmarkId, bookmark);
+        return new BookmarksTree(this.rootFolderId, newBookmarksMap, this.childrenMap, this.parentMap);
+    }
+
+    public deleteBookmark(bookmarkId : BookmarkId, recurse : boolean = false) : BookmarksTree {
+        const bookmark = this.getBookmark(bookmarkId);
+        if (!bookmark) {
+            return this;
+        }
+        this.checkNotRootFolder(bookmarkId);
+        let newBookmarksTree : BookmarksTree = this;
+        if (recurse && bookmark instanceof BookmarkFolder) {
+            newBookmarksTree = this.deleteBookmarksUnder(bookmarkId);
+            return newBookmarksTree.deleteBookmark(bookmarkId);
+        }
+        if (this.childrenMap.hasChildren(bookmarkId)) {
+            throw new Error('Cannot delete non-empty folder');
+        }
+        const parentFolder = this.getParentBookmark(bookmarkId)!;
+        return new BookmarksTree(
+            this.rootFolderId, 
+            this.bookmarksMap.delete(bookmarkId), 
+            this.childrenMap.delete(parentFolder.bookmarkId, bookmarkId), 
+            this.parentMap.delete(bookmarkId));
+    }
+
+    private deleteBookmarksUnder(bookmarkFolderId : BookmarkId) : BookmarksTree {
+        const subTreeBookmarks = new Set<Bookmark>();
+        this.getAllBookmarksUnder(bookmarkFolderId, subTreeBookmarks);
+        let newChildrenMap = this.childrenMap;
+        let newParentMap = this.parentMap;
+        let newBookmarksMap = this.bookmarksMap;
+        subTreeBookmarks.forEach(bookmark => {
+            newChildrenMap = newChildrenMap.delete(bookmark.bookmarkId);
+            newParentMap = newParentMap.delete(bookmark.bookmarkId);
+            newBookmarksMap = newBookmarksMap.delete(bookmark.bookmarkId);
+        });
+        newChildrenMap = newChildrenMap.delete(bookmarkFolderId);
+        return new BookmarksTree(this.rootFolderId, newBookmarksMap, newChildrenMap, newParentMap);
+    }
+
+    private getAllBookmarksUnder(bookmarkFolderId : BookmarkId, bookmarks : Set<Bookmark>) {
+        const children = this.childrenMap.getChildren(bookmarkFolderId)
+            .map(bookmarkId => this.getBookmark(bookmarkId)!);
+        children.forEach(bookmark => {
+            bookmarks.add(bookmark);
+            if (bookmark instanceof BookmarkFolder) {
+                this.getAllBookmarksUnder(bookmark.bookmarkId, bookmarks);
+            }
+        });
+    }
+
     private getBookmarkIds(bookmarks: Iterable<Bookmark>): List<BookmarkId> {
-        return List().withMutations(list=> {
+        return List().withMutations(list => {
             for (let bookmark of bookmarks) {
                 list.push(bookmark.bookmarkId);
             }
@@ -103,6 +189,12 @@ export class BookmarksTree {
         }
     }
 
+    private checkNotRootFolder(bookmarkId : BookmarkId) {
+        if (this.rootFolderId === bookmarkId) {
+            throw new Error("Operation invalid on root folder");
+        }
+    }
+
 }
 
 class BookmarksChildrenMap {
@@ -124,8 +216,21 @@ class BookmarksChildrenMap {
     }
 
 
-    public delete(bookmarkId: BookmarkId): BookmarksChildrenMap {
-        return this.createBookmarksChildrenMap(this.idToChildren.delete(bookmarkId));
+    public delete(parentId : BookmarkId, bookmarkId? : BookmarkId) : BookmarksChildrenMap {
+        if (!bookmarkId) {
+            return this.createBookmarksChildrenMap(this.idToChildren.delete(parentId));
+        }
+        let newChildrenList = this.idToChildren.get(parentId);
+        if (!newChildrenList) {
+            return this;
+        }
+        const index = newChildrenList.findIndex(value => value === bookmarkId);
+        newChildrenList = newChildrenList.delete(index);
+        if (newChildrenList.isEmpty()) {
+            return new BookmarksChildrenMap(this.idToChildren.delete(parentId));
+        } else {
+            return new BookmarksChildrenMap(this.idToChildren.set(parentId, newChildrenList));
+        }
     }
 
     public add(parentId: BookmarkId, bookmarkIds: List<BookmarkId>, index?: number): BookmarksChildrenMap {
@@ -140,12 +245,16 @@ class BookmarksChildrenMap {
             }
         } else {
             children = children.push(...bookmarkIds.toArray());
-        } 
+        }
         return this.createBookmarksChildrenMap(this.idToChildren.set(parentId, children));
     }
 
     public getChildren(parentId: BookmarkId): List<BookmarkId> {
         return this.idToChildren.get(parentId, List());
+    }
+
+    public hasChildren(parentId: BookmarkId) : boolean {
+        return this.idToChildren.has(parentId);
     }
 
 }
--- packages/core/src/model/__tests__/BookmarksTree.test.ts
+++ packages/core/src/model/__tests__/BookmarksTree.test.ts
@@ -1,10 +1,10 @@
 import { BookmarksTree } from '../BookmarksTree';
-import { Bookmark, BookmarkFolder } from '../Bookmark';
-import { List } from 'immutable';
+import { Bookmark, BookmarkFolder, getBookmarkId } from '../Bookmark';
+import { List, Map } from 'immutable';
 
 describe('BookmarksTree Tests', () => {
-    let bookmarksTree : BookmarksTree;
-    const rootFolder = BookmarkFolder.bookmarkFolder("rootId", "root");
+	let bookmarksTree: BookmarksTree;
+	const rootFolder = BookmarkFolder.bookmarkFolder("rootId", "root");
 	const bookmarkFolder1 = BookmarkFolder.bookmarkFolder("folder1Id", "folder1");
 	const bookmarkFolder2 = BookmarkFolder.bookmarkFolder("folder2Id", "folder2");
 	const bookmark1 = new Bookmark("id1");
@@ -14,27 +14,27 @@ describe('BookmarksTree Tests', () => {
 	const bookmark5 = new Bookmark("id5");
 	const bookmark6 = new Bookmark("id6");
 
-    beforeEach(() => {
+	beforeEach(() => {
 		bookmarksTree = BookmarksTree.bookmarksTree(rootFolder);
 		bookmarksTree = bookmarksTree.addBookmarks(rootFolder.bookmarkId, [bookmarkFolder1, bookmarkFolder2], 0);
 		bookmarksTree = bookmarksTree.addBookmarks(bookmarkFolder1.bookmarkId, [bookmark1, bookmark2, bookmark3], 0);
 		bookmarksTree = bookmarksTree.addBookmarks(bookmarkFolder2.bookmarkId, [bookmark4, bookmark5, bookmark6], 0);
-    });
-	
-	describe('getParent' , () => {
+	});
+
+	describe('getParent', () => {
 		test('should return undefined for the root folder', () => {
-            // Given
+			// Given
 			let actualRootFolder = bookmarksTree.getBookmarkFolder(bookmarksTree.rootFolderId);
 			expect(actualRootFolder).toBeDefined;
 			actualRootFolder = actualRootFolder!;
 
-		    // When
-		    const parent = bookmarksTree.getParentBookmark(actualRootFolder.bookmarkId);
+			// When
+			const parent = bookmarksTree.getParentBookmark(actualRootFolder.bookmarkId);
 
 			// Then
 			expect(parent).toBeUndefined();
-        });
-		test('should return the parent bookmark folder' , () => {
+		});
+		test('should return the parent bookmark folder', () => {
 			// When
 			let bookmarkFolder = bookmarksTree.getParentBookmark(bookmark6.bookmarkId);
 
@@ -65,12 +65,108 @@ describe('BookmarksTree Tests', () => {
 			// When
 			bookmarksTree = bookmarksTree.addBookmarks(bookmarkFolder1.bookmarkId, [bookmark7, bookmark8]);
 
+			// Then
 			expect(bookmarksTree.getChildren(bookmarkFolder1.bookmarkId)).toEqual(List([bookmark1, bookmark2, bookmark3, bookmark7, bookmark8]));
 			expect(bookmarksTree.getParentBookmark(bookmark7.bookmarkId)).toEqual(bookmarkFolder1);
 			expect(bookmarksTree.getParentBookmark(bookmark8.bookmarkId)).toEqual(bookmarkFolder1);
 		});
 		test('should throw an exception if we try to add the same bookmark twice', () => {
 			expect(() => bookmarksTree.addBookmarks(bookmarkFolder2.bookmarkId, [bookmark1])).toThrow(Error('Bookmark already in tree'));
+		});
+	});
+	describe('setPropertyValue', () => {
+		test('set the property value', () => {
+			// When
+			bookmarksTree = bookmarksTree.setPropertyValue(bookmark1.bookmarkId, 'prop1', 'value1');
+			bookmarksTree = bookmarksTree.setPropertyValue(bookmark1.bookmarkId, 'prop2', 'value2');
+
+			// Then
+			const newBookmark = bookmarksTree.getBookmark(bookmark1.bookmarkId)!;
+			expect(newBookmark.properties.get('prop1')).toEqual('value1');
+			expect(newBookmark.properties.get('prop2')).toEqual('value2');
+		});
+		test('should return the same bookmarksTree when we set the same property value than existing one', () => {
+			// Given
+			bookmarksTree = bookmarksTree.setPropertyValue(bookmark1.bookmarkId, 'prop1', 'value1');
+			bookmarksTree = bookmarksTree.setPropertyValue(bookmark1.bookmarkId, 'prop2', 'value2');
+
+			// When
+			const newBookmarksTree = bookmarksTree.setPropertyValue(bookmark1.bookmarkId, 'prop1', 'value1');
+
+			// Then
+			expect(newBookmarksTree).toBe(bookmarksTree);
+		});
+		test('delete a property', () => {
+			// Given
+			bookmarksTree = bookmarksTree.setPropertyValue(bookmark1.bookmarkId, 'prop1', 'value1');
+			bookmarksTree = bookmarksTree.setPropertyValue(bookmark1.bookmarkId, 'prop2', 'value2');
+
+			// When
+			const newBookmarksTree = bookmarksTree.setPropertyValue(bookmark1.bookmarkId, 'prop1', undefined);
+
+			// Then
+			const newBookmark = newBookmarksTree.getBookmark(bookmark1.bookmarkId)!;
+			expect(newBookmark.properties.get('prop1')).toBeUndefined();
+			expect(newBookmark.properties.get('prop2')).toEqual('value2');
+		});
+	});
+	describe('setProperties', () => {
+		test('set properties for a bookmark', () => {
+			// Given
+			const properties = Map({ prop1: 'value1', prop2: 'value2'});
+
+			// When
+			bookmarksTree = bookmarksTree.setProperties(bookmark1.bookmarkId, properties);
+
+			// Then
+			const bookmark = bookmarksTree.getBookmark(bookmark1.bookmarkId)!;
+			expect(bookmark.properties.get('prop1')).toEqual('value1');
+			expect(bookmark.properties.get('prop2')).toEqual('value2');			
+		});
+		test('should return the same bookmarksTree if we set the same properties than the existing ones', () => {
+			// Given
+			bookmarksTree = bookmarksTree.setPropertyValue(bookmark1.bookmarkId, 'prop1', 'value1');
+			bookmarksTree = bookmarksTree.setPropertyValue(bookmark1.bookmarkId, 'prop2', 'value2');
+
+			// When
+			const newBookmarksTree = bookmarksTree.setProperties(bookmark1.bookmarkId, Map({ prop1: 'value1', prop2: 'value2'}));
+
+			// Then
+			expect(newBookmarksTree).toBe(bookmarksTree);			
+		});
+	});
+	describe('deleteBookmark', () => {
+		it('deletes a single bookmark', () => {
+			// When
+			bookmarksTree = bookmarksTree.deleteBookmark(bookmark2.bookmarkId, false);
+
+			// Then
+			expect(bookmarksTree.getBookmark(bookmark2.bookmarkId)).toBeUndefined();
+			expect(bookmarksTree.getChildren(bookmarkFolder1.bookmarkId)).toEqual(List([bookmark1, bookmark3]));
+		});
+		test('should throw an exception if we try to delete a non-empty folder without recurse', () => {
+			expect(() => bookmarksTree.deleteBookmark(bookmarkFolder2.bookmarkId)).toThrow(Error('Cannot delete non-empty folder'));
+		});
+		it('deletes a bookmark folder recursively when recurse is true', () => {
+			// Given
+			const bookmarkFolder3 = BookmarkFolder.bookmarkFolder('folder3Id', 'folder3');
+			const bookmark7 = new Bookmark('id7');
+			const bookmark8 = new Bookmark('id8');
+			bookmarksTree = bookmarksTree.addBookmarks(bookmarkFolder2.bookmarkId, [bookmarkFolder3]);
+			bookmarksTree = bookmarksTree.addBookmarks(bookmarkFolder3.bookmarkId, [bookmark7, bookmark8]);
+
+			// When
+			debugger;
+			bookmarksTree = bookmarksTree.deleteBookmark(bookmarkFolder2.bookmarkId, true);
+
+			// Then
+			expect(bookmarksTree.getBookmark(bookmarkFolder2.bookmarkId)).toBeUndefined();
+			expect(bookmarksTree.getBookmark(bookmark4.bookmarkId)).toBeUndefined();
+			expect(bookmarksTree.getBookmark(bookmark5.bookmarkId)).toBeUndefined();
+			expect(bookmarksTree.getBookmark(bookmark6.bookmarkId)).toBeUndefined();
+			expect(bookmarksTree.getBookmark(bookmarkFolder3.bookmarkId)).toBeUndefined();
+			expect(bookmarksTree.getBookmark(bookmark7.bookmarkId)).toBeUndefined();
+			expect(bookmarksTree.getBookmark(bookmark8.bookmarkId)).toBeUndefined();
 		});
 	});
 });
